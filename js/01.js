4205cb46975b0fb7db258bd3c665843fded9439a
Administrator <569485266@qq.com>
2019-10-08 17:25:15 +0800

    2019.10.8 update code

--- js/01.js
+++ js/01.js
@@ -1,36 +1,19 @@
- window.onload = function () {
-            var timer = null;
-            var isTop = true;
-            var go_top = document.getElementById('go_top');
-            window.onscroll = function () {
-            var h = window.document.body.scrollHeight || window.document.documentElement.scrollHeight;
-            var clientHeight = window.document.body.clientHeight || window.document.documentElement.clientHeight;
-                if (h >= clientHeight) {
-                    go_top.style.display = 'block';
-                } else {
-                    go_top.style.display = 'none';
-                }
-
-                if (!isTop) {
-                    if (timer != null) {
-                        clearInterval(timer);
-                    }
-                }
-                isTop = false;
-            }
-
-            go_top.onclick = function () {
-                timer = setInterval(function () {
-                    var h = window.document.body.scrollTop || window.document.documentElement.scrollTop;
-                    h = (h / 4);
-                    window.document.body.scrollTop -= h;
-                    window.document.documentElement.scrollTop -= h;
-
-                    if (h <= 0) {
-                        clearInterval(timer);
-                        go_top.style.display = 'none';
-                    }
-                    isTop = true;
-                }, 50);
-            };
+ $(document).ready(function () {
+    var toTop = $("#go_top");
+    if ($(window).scrollTop() < 400) {
+        toTop.hide();
+    }
+    $(window).scroll(function () {
+        if ($(window).scrollTop() < 400) {
+            toTop.fadeOut("fast");
+        } else {
+            toTop.fadeIn("fast");
         }
+    });
+    toTop.click(function () {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 500);
+        return false
+    });
+});
